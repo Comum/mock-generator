7860e9ee73e04e52015edea10d9f5cf17aff8b69
ribeirom <miguel.ribeiro@paddypowerbetfair.com>
2019-08-13 10:46:26 +0100

    chore: first commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const app = express();
+const path = require('path');
+
+const { fetchDirectories, getLastFolder, getAllSchemasInFolders } = require('./src/library/utils/file-utils');
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.render('pages/index');
+});
+
+app.post('/form-response', (req, res) => {
+    res.send(`test: ${req.body.test}`);
+});
+
+app.get('/schemas', (req, res) => {
+    const fullDirectories = fetchDirectories(path.join(__dirname + '/src/responses/'));
+    const folders = getLastFolder(fullDirectories);
+
+    getAllSchemasInFolders(folders).then((response) => {
+        res.render('pages/schemas', { structure: response }, (err, html) => {
+            if (err) {
+                res.render('pages/notFound', { err });
+            } else {
+                res.send(html);
+            }
+        });
+    });
+});
+
+app.get('/schemas-json/:folder/:file', function(req, res) {
+    if (req.params.folder && req.params.file) {
+        rawData = fs.readFileSync(path.join(__dirname + '/src/responses/' + req.params.folder + '/schemas/' + req.params.file + '.json'));
+        parsedData = JSON.parse(rawData);
+        res.send(parsedData);
+    } else {
+        res.render('pages/notFound', { err: 'File not found' });
+    }
+});
+
+app.listen(8000, () => {
+    console.log('Example app listening on port 8000!');
+});
---
+++ src/library/utils/file-utils.js
@@ -0,0 +1,77 @@
+const fs = require("fs");
+const path = require("path");
+
+const fetchDirectories = (mainSource) => {
+    const isDirectory = (source) => fs.lstatSync(source).isDirectory();
+    const getDirectories = (source) =>
+        fs
+            .readdirSync(source)
+            .map((name) => path.join(source, name))
+            .filter(isDirectory);
+
+    return getDirectories(mainSource);
+};
+
+const getLastFolder = (directories) => {
+    const lastFolder = [];
+
+    directories.forEach((dir) => {
+        const folders = dir.split("/");
+        const foldersLength = folders.length;
+
+        lastFolder.push(folders[foldersLength - 1]);
+    });
+
+    return lastFolder;
+};
+
+const getFilesInFolder = (folder) => {
+    return new Promise((resolve, reject) => {
+        fs.readdir(folder, (err, files) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(files);
+            }
+        });
+    });
+};
+
+const getAllSchemasInFolders = (folders) => {
+    const structure = {
+        folders: []
+    };
+    const folderLength = folders.length;
+
+    return new Promise((resolve, reject) => {
+        folders.forEach((folder, index) => {
+            let files = [];
+            
+            getFilesInFolder("./src/responses/" + folder + "/schemas")
+                .then((response) => {
+                    response.forEach((file) => {
+                        files.push(file.split('.json')[0]);
+                    });
+    
+                    structure.folders.push({
+                        folder,
+                        files
+                    });
+
+                    if ((index + 1) === folderLength) {
+                        resolve(structure);
+                    }
+                })
+                .catch((err) => {
+                    console.log('Error: ', err);
+                });
+        });
+    });
+}
+
+module.exports = {
+    fetchDirectories,
+    getLastFolder,
+    getFilesInFolder,
+    getAllSchemasInFolders
+};
