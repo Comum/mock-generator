50717603db942272040d1743e6ed7f4df777bca7
ribeirom <miguel.ribeiro@paddypowerbetfair.com>
2019-08-20 11:14:46 +0100

    chore: add test route

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    const testResponse = {
+        hello: 'world'
+    };
+    const stringifiedObj = JSON.stringify(testResponse);
+    const parsedObj = JSON.parse(stringifiedObj);
+
+    res.send(testResponse);
+});
+
+app.listen(8000, () => {
+    console.log("Example app listening on port 8000!");
+});
