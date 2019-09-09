de0f9811215fb06045892cf6b9db996625381320
Comum <miguel.rib.20@gmail.com>
2019-09-09 19:26:20 +0100

    chore: add function to create buttons

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,55 @@ import axios from "axios";
 
 const path = "http://localhost:8000/supported-requests";
 
+const getMocks = (folders) => {
+    let mocks = [];
+
+    if (!folders.children) {
+        return mocks;
+    }
+
+    folders.children.forEach((folder) => {
+        mocks.push(folder.name);
+    });
+    return mocks;
+};
+
+const getMockButtons = (mocks) => {
+    if (!mocks.length) {
+        return (<div>No mocks available</div>);
+    }
+
+    return mocks.map((mock, index) => { 
+        return (<div key={index}>{mock}</div>);
+    });
+};
+
 function App() {
     const [folders, setFolders] = useState([]);
+    let mocks = [];
+    let mocksButtons = [];
 
     useEffect(() => {
         const fetchData = async () => {
             const result = await axios(path);
 
-            console.log(result);
             setFolders(result.data);
         };
         fetchData();
     }, []);
+
+    if (Object.keys(folders).length) {
+        mocks = getMocks(folders);
+    }
+
+    mocksButtons = getMockButtons(mocks);
 
     return (
         <div>
             <h2>Mock generator</h2>
             <div>
                 <h3>Pick your mock</h3>
-                <div>{folders.children && folders.children[0].name}</div>
+                <div>{mocksButtons}</div>
             </div>
             <div>
                 <h3>Mock area</h3>
