a7965db7d4e3868649cb1958db49e25b487f9c22
ribeirom <miguel.ribeiro@paddypowerbetfair.com>
2019-11-06 08:43:57 +0000

    chore: import json files

--- server/lib/event-page-generator.js
+++ server/lib/event-page-generator.js
@@ -1,5 +1,9 @@
+const eventPageTemplate = require("../constants/mock-templates/event-page-template.json");
+const getMarketPricesTemplate = require("../constants/mock-templates/get-market-prices-template.json");
+
 const generateEventPageMock = function (data) {
     console.log("data", data);
+    console.log("eventPageTemplate", eventPageTemplate);
 
     return {
         sucess: "Generating event page mock"
