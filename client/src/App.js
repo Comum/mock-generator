e9fd2f7869954f436ba851f71665fd498592f625
Comum <miguel.rib.20@gmail.com>
2019-11-20 20:37:03 +0000

    chore: clear paths and change css min-width

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,26 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import Header from "./components/Header/Header";
-import MockArea from "./components/MockArea/MockArea";
-import MockSelecter from "./components/MockSelector/MockSelector";
+import Header from './components/Header/Header';
+import MockArea from './components/MockArea/MockArea';
+import MockSelecter from './components/MockSelector/MockSelector';
 
-import { EMPTY_MOCK_TITLE } from "./constants/constants";
+import { EMPTY_MOCK_TITLE } from './constants/constants';
 
 function App() {
-    const [pageType, setPageType] = useState(EMPTY_MOCK_TITLE);
-    const [requestPath, setRequestPath] = useState("");
-    const onClickMock = (mock, mockRequestPath) => {
-        setPageType(mock);
-        setRequestPath(mockRequestPath);
-    };
+	const [pageType, setPageType] = useState(EMPTY_MOCK_TITLE);
+	const [requestPath, setRequestPath] = useState('');
+	const onClickMock = (mock, mockRequestPath) => {
+		setPageType(mock);
+		setRequestPath(mockRequestPath);
+	};
 
-    return (
-        <div>
-            <Header />
-            <MockSelecter onClickCallback={onClickMock} />
-            <MockArea pageType={pageType} requestPath={requestPath} />
-        </div>
-    );
+	return (
+		<div>
+			<Header />
+			<MockSelecter onClickCallback={onClickMock} />
+			<MockArea pageType={pageType} requestPath={requestPath} />
+		</div>
+	);
 }
 
 export default App;
--- client/src/components/mockSelector/mockSelector.js
+++ client/src/components/mockSelector/mockSelector.js
@@ -1,73 +1,73 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-import { SUPPORTED_REQUEST_PATH } from "../../constants/constants";
-import { getRequestPath } from "../../utils/utils";
+import { SUPPORTED_REQUEST_PATH } from '../../constants/constants';
+import { getRequestPath } from '../../utils/utils';
 
-const getMocks = (folders) => {
-    let mocks = [];
+const getMocks = folders => {
+	let mocks = [];
 
-    if (!folders.children) {
-        return mocks;
-    }
+	if (!folders.children) {
+		return mocks;
+	}
 
-    folders.children.forEach((folder) => {
-        mocks.push(folder.name);
-    });
-    return mocks;
+	folders.children.forEach(folder => {
+		mocks.push(folder.name);
+	});
+	return mocks;
 };
 
 const getMockButtons = (mocks, onClickCallback) => {
-    if (!mocks.length) {
-        return <div>No mocks available</div>;
-    }
+	if (!mocks.length) {
+		return <div>No mocks available</div>;
+	}
 
-    return mocks.map((mock, index) => {
-        const mockRequestPath = getRequestPath(mock);
-        
-        return (
-            <div
-                key={index}
-                onClick={() => {
-                    onClickCallback(mock, mockRequestPath);
-                }}
-            >
-                {mock}
-            </div>
-        );
-    });
+	return mocks.map((mock, index) => {
+		const mockRequestPath = getRequestPath(mock);
+
+		return (
+			<div
+				key={index}
+				onClick={() => {
+					onClickCallback(mock, mockRequestPath);
+				}}
+			>
+				{mock}
+			</div>
+		);
+	});
 };
 
 function MockSelector(props) {
-    const { onClickCallback } = props;
-    const [folders, setFolders] = useState([]);
+	const { onClickCallback } = props;
+	const [folders, setFolders] = useState([]);
 
-    let mocks = [];
-    let mocksButtons = [];
+	let mocks = [];
+	let mocksButtons = [];
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const result = await axios(SUPPORTED_REQUEST_PATH);
+	useEffect(() => {
+		const fetchData = async () => {
+			const result = await axios(SUPPORTED_REQUEST_PATH);
 
-            setFolders(result.data);
-        };
-        fetchData();
-    }, []);
+			setFolders(result.data);
+		};
+		fetchData();
+	}, []);
 
-    if (Object.keys(folders).length) {
-        mocks = getMocks(folders);
-    }
+	if (Object.keys(folders).length) {
+		mocks = getMocks(folders);
+	}
 
-    mocksButtons = getMockButtons(mocks, onClickCallback);
+	mocksButtons = getMockButtons(mocks, onClickCallback);
 
-    return (
-        <div>
-            <div>
-                <h3>Pick your mock</h3>
-                <div>{mocksButtons}</div>
-            </div>
-        </div>
-    );
+	return (
+		<div>
+			<div>
+				<h3>Pick your mock</h3>
+				<div>{mocksButtons}</div>
+			</div>
+		</div>
+	);
 }
 
 export default MockSelector;
